a0dfebe55adfec075d4621c5eabf98bea2ac79d4
Dhiraj Magdum <dhiraj.magdum@outscal.com>
2024-02-04 20:00:55 +0530

    Corrected player movement

--- Space-Invaders/source/Event/EventService.cpp
+++ Space-Invaders/source/Event/EventService.cpp
@@ -21,6 +21,10 @@ namespace Event
     {
         updateMouseButtonsState(left_mouse_button_state, sf::Mouse::Left);
         updateMouseButtonsState(right_mouse_button_state, sf::Mouse::Right);
+        updateKeyboardButtonsState(left_arrow_button_state, sf::Keyboard::Left);
+        updateKeyboardButtonsState(right_arrow_button_state, sf::Keyboard::Right);
+        updateKeyboardButtonsState(A_button_state, sf::Keyboard::A);
+        updateKeyboardButtonsState(D_button_state, sf::Keyboard::D);
     }
 
     void EventService::processEvents()
@@ -56,6 +60,26 @@ namespace Event
         }
     }
 
+    void EventService::updateKeyboardButtonsState(ButtonState& current_button_state, sf::Keyboard::Key keyboard_button)
+    {
+        if (sf::Keyboard::isKeyPressed(keyboard_button))
+        {
+            switch (current_button_state)
+            {
+            case ButtonState::RELEASED:
+                current_button_state = ButtonState::PRESSED;
+                break;
+            case ButtonState::PRESSED:
+                current_button_state = ButtonState::HELD;
+                break;
+            }
+        }
+        else
+        {
+            current_button_state = ButtonState::RELEASED;
+        }
+    }
+
     bool EventService::isGameWindowOpen() { return game_window != nullptr; }
 
     bool EventService::gameWindowWasClosed() { return game_event.type == sf::Event::Closed; }
@@ -66,9 +90,13 @@ namespace Event
 
     bool EventService::pressedEscapeKey() { return game_event.key.code == sf::Keyboard::Escape; }
 
-    bool EventService::pressedLeftArrowKey() { return (isKeyboardEvent() && game_event.key.code == sf::Keyboard::Left); }
+    bool EventService::pressedLeftArrowKey() { return left_arrow_button_state == ButtonState::HELD; }
 
-    bool EventService::pressedRightArrowKey() { return  (isKeyboardEvent() && game_event.key.code == sf::Keyboard::Right); }
+    bool EventService::pressedRightArrowKey() { return right_arrow_button_state == ButtonState::HELD; }
+
+    bool EventService::pressedAKey() { return A_button_state == ButtonState::HELD; }
+
+    bool EventService::pressedDKey() { return D_button_state == ButtonState::HELD; }
 
     bool EventService::pressedLeftMouseButton() { return left_mouse_button_state == ButtonState::PRESSED; }
 
