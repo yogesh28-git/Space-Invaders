c550cb054c2c2468ac67a3f20a06c31169037224
Yogesh <yogeshsuresh28@gmail.com>
2024-07-07 21:46:19 +0530

    Player key press and movement

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,28 +1,32 @@
 #include <SFML/Graphics.hpp>
 
 
-class Player
+class PlayerShip
 {
 
 private:
-    sf::Texture playerTexture;
-    sf::Sprite playerSprite;
     int health = 3;
     int movementSpeed = 10;
     sf::Vector2f position{ 400,500 };
 
 public:
+    sf::Texture player_texture;
+    sf::Sprite player_sprite;
     void takeDamage()
     {
 
     }
-    void move()
+    void move(float x, float y)
     {
-        
+        position = position + sf::Vector2f(x, y);
     }
     void shootBullets()
     {
 
+    }
+    sf::Vector2f getPosition()
+    {
+        return position;
     }
 };
 
@@ -32,7 +36,17 @@ int main()
     sf::VideoMode videoMode(800, 600);
     sf::RenderWindow window(videoMode, "Space-Invaders");
     window.setFramerateLimit(60);
-    sf::Vector2f v1(16.5f, 24.f);
+    
+
+    //Player
+    PlayerShip player;
+    if (!player.player_texture.loadFromFile("assets/textures/player_ship.png"))
+    {
+        printf("Could not load player texture");
+        return 0;
+    }
+    player.player_sprite.setTexture(player.player_texture);
+
 
     while (window.isOpen())
     {
@@ -42,9 +56,23 @@ int main()
         {
             if (event.type == sf::Event::EventType::Closed)
                 window.close();
+            
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+        {
+            player.move(-1.0f,0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+        {
+            player.move(1.0f,0);
         }
 
         window.clear(sf::Color::Black);
+
+        player.player_sprite.setPosition(player.getPosition());
+
+        window.draw(player.player_sprite);
 
         window.display();
     }
