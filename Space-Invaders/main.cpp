be5072703aef01196f875ea52c5905d5504eebe2
Yogesh <yogeshsuresh28@gmail.com>
2025-01-14 16:12:36 +0530

    Player Data filled

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -2,42 +2,44 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+class Player
+{
+private:
+    
+    int player_score = 0;
+    int health = 3;
+    int movement_speed = 5;
+    sf::Vector2f position = sf::Vector2f(200, 100);
+
+public:
+
+    sf::Texture player_texture;
+    sf::Sprite  player_sprite;
+
+    void takeDamage();
+    void move();
+    void shootBullets();
+};
+
 int main()
 {
-    //sf::RenderWindow window(sf::VideoMode(800,800), "SFML Works !!");
-    sf::RenderWindow window(sf::VideoMode(1000, 800), "Resizable Window", sf::Style::Resize | sf::Style::Close);
-    //sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Fullscreen Window", sf::Style::Fullscreen | sf::Style::Close);
+    sf::VideoMode videoMode = sf::VideoMode(800, 800);
+    sf::RenderWindow window(videoMode, "Resizable Window", sf::Style::Resize | sf::Style::Close);
 
-
-    sf::Texture outscal_logo_tex;
-    if (!outscal_logo_tex.loadFromFile("assets/textures/outscal_logo.png"))
-    {
-        std::cout<<"Texture did not load";
-    }
-    sf::Sprite outscal_logo;
-    outscal_logo.setTexture(outscal_logo_tex);
-    outscal_logo.setPosition(10, 10);
-
-    sf::Font font;
-    font.loadFromFile("assets/fonts/bubbleBobble.ttf");
-
-    sf::Text text("Hello Fucking World!!", font, 50);
-    text.setPosition(10, 150);
     
+
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::EventType::Closed)
                 window.close();
-        }
 
-        window.clear();
-        window.draw(outscal_logo);
-        window.draw(text);
-        window.display();
+
+            window.clear(sf::Color::Blue);
+            window.display();
+        }
     }
-    
     return 0;
 }
