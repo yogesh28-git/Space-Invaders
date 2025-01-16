f3b4d4b55157bb3583a9261204790791014b8d83
Yogesh <yogeshsuresh28@gmail.com>
2025-01-16 09:40:21 +0530

    Implemented Player Model and enums

---
+++ Space-Invaders/Source/PlayerModel.cpp
@@ -0,0 +1,52 @@
+#include "../Header/PlayerModel.h"
+
+PlayerModel::PlayerModel()
+{
+}
+
+PlayerModel::~PlayerModel()
+{
+}
+
+void PlayerModel::initialize()
+{
+	reset();
+}
+
+void PlayerModel::reset()
+{
+	player_state = PlayerState::ALIVE;
+	player_position = initial_position;
+	player_score = 0;
+}
+
+sf::Vector2f PlayerModel::getPlayerPosition()
+{
+	return player_position;
+}
+
+void PlayerModel::setPlayerPosition(sf::Vector2f position)
+{
+	player_position = position;
+}
+
+int PlayerModel::getPlayerScore()
+{
+	return player_score;
+}
+
+void PlayerModel::setPlayerScore(int score)
+{
+	player_score = score;
+}
+
+PlayerState PlayerModel::getPlayerState()
+{
+	return player_state;
+}
+
+void PlayerModel::setPlayerState(PlayerState state)
+{
+	player_state = state;
+}
+
